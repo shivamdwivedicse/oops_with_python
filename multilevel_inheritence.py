e372e007b7e2003e7bae048f529cbb38488d74a3
Shivam Dwivedi <shivamdwivedicse20919@gmail.com>
2026-03-12 22:54:52 +0530

    Added example of multilvel inheritence

---
+++ multilevel_inheritence.py
@@ -0,0 +1,17 @@
+class Factory:
+    def __init__(self,material,zip):
+        self.material = material
+        self.zips = zips
+
+class BhopalFactory(Factory):
+    def __init__(self, material, zip,color):
+        super().__init__(material, zip)
+        self.color = color
+
+class PuneFactory(BhopalFactory):
+    def __init__(self, material, zip, color,pockets):
+        super().__init__(material, zip, color)
+        self.pockets = pockets
+
+
+obj = PuneFactory()
